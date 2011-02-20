973f48c1624aac9e7db77731d1118ed9ce4244e8
Djalma Oliveira <djalmaoliveira@gmail.com>
2011-02-20 10:22:05 -0300

    add: SiteMap Class.

---
+++ tests/Classes/SiteMapTest.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once dirname(dirname(__FILE__)).'/include.php';
+
+class SiteMapTest extends PHPUnit_Framework_TestCase {
+
+
+
+    function SetUp() {
+        Liber::loadClass('SiteMap');
+    }
+
+    function tearDown() {
+    }
+
+    function testGetArrayUrl() {
+        $s = new SiteMap;
+		$s->url( Array('loc'=>'http://url.com.br') );
+		$urls = $s->url();
+
+        $this->assertTrue( count($urls) > 0, "Should have some url." );
+    }
+
+    function testXmlOutPut() {
+        $s = new SiteMap;
+		$s->url( Array('loc'=>'http://url.com.br', 'lastmod'=>'2010-11-01') );
+		$xml = $s->xml();
+file_put_contents('/home/usuario/Desktop/s.xml', $xml);
+        $this->assertTrue( !empty($xml), "Should have XML data." );
+    }
+
+}
+?>
